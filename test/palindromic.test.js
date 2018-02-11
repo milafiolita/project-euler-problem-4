9d7d886d502b8f96b1648692e95fff620e1d9aa7
Mila Fiolita <milafiolita01@gmail.com>
2018-02-11 20:54:02 +0700

    Add existing file

---
+++ test/palindromic.test.js
@@ -0,0 +1,5 @@
+const palindromic = require('../src/palindromic');
+
+test('The largest palindrome made from the product of two 3-digit numbers is ', () => {
+  expect(palindromic).toBe(906609);
+})
